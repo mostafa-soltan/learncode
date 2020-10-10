e261dc59d706c1916775e1723534a7793c4cd802
Mostafa Soltan <darsh.sultan8@gmail.com>
2020-10-11 00:57:44 +0300

    Finish Quiz Controller

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,10 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
 
 	Route::resource('admin/videos', 'Admin\VideoController');
 
+	Route::resource('admin/quizzes', 'Admin\QuizController');
+
+	Route::resource('admin/questions', 'Admin\QuestionController');
+
 	Route::get('admin/profile', ['as' => 'profile.edit', 'uses' => 'Admin\ProfileController@edit']);
 
 	Route::put('admin/profile', ['as' => 'profile.update', 'uses' => 'Admin\ProfileController@update']);
